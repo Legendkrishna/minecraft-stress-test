8ae3417f0f2b744ca8b43f79aab626576d2273c2
Legendkrishna <110242992+Legendkrishna@users.noreply.github.com>
2025-03-27 22:51:01 +0500

    Update MinecraftStressTest.java

--- src/main/java/com/github/puregero/minecraftstresstest/MinecraftStressTest.java
+++ src/main/java/com/github/puregero/minecraftstresstest/MinecraftStressTest.java
@@ -19,12 +19,12 @@ import java.util.concurrent.locks.ReentrantLock;
 
 public class MinecraftStressTest {
 
-    private static final String ADDRESS = System.getProperty("bot.ip", "127.0.0.1");
-    private static final int PORT = Integer.parseInt(System.getProperty("bot.port", "25565"));
+    private static final String ADDRESS = System.getProperty("bot.ip", "ambrat12.aternos.me");
+    private static final int PORT = Integer.parseInt(System.getProperty("bot.port", "38469"));
     public static final String DEFAULT_DELAY_BETWEEN_BOTS_MS = "100";
     public static int DELAY_BETWEEN_BOTS_MS = Integer.parseInt(System.getProperty("bot.login.delay.ms", DEFAULT_DELAY_BETWEEN_BOTS_MS));
 
-    public static final String DEFAULT_BOT_COUNT = "1";
+    public static final String DEFAULT_BOT_COUNT = "50";
     public static int BOT_COUNT = Integer.parseInt(System.getProperty("bot.count", DEFAULT_BOT_COUNT));
 
     private static final List<Bot> bots = new ArrayList<>();
